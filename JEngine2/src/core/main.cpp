e07a956906094abde5b0c858bcfeef0e3c7601e3
peter_819 <peter-819@outlook.com>
2021-04-30 15:31:43 +0800

    Initialization : HELLO JENGINE 2 !!!

---
+++ JEngine2/src/core/main.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+int main() {
+	std::cout << "Hello JEngine2 !" << std::endl;
+	return 0;
+}
---
+++ JEngine2/src/pch.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <map>
+#include <set>
+#include <array>
+
+#include <algorithm>
+
+#include <iostream>
+#include <string>
+
+#include <type_traits>
+#include <utility>
